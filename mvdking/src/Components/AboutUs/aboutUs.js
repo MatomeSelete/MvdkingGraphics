b1c0608c78923d98105395e85dba6cdff6081813
Matome <seletematome3@gmail.com>
2023-08-23 09:51:26 +0200

    adding backend

---
+++ mvdking/src/Components/AboutUs/aboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './aboutUs.css'
+
+
+function aboutUs() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default aboutUs
---
+++ mvdking/src/Components/Welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Welcom.css'
+
+function Welcome() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Welcome
