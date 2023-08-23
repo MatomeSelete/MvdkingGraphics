d3c5a47d5f991f52c3b170a1a145ed2e5fb56e7e
Matome <seletematome3@gmail.com>
2023-08-23 15:36:23 +0200

    contact page is working

--- mvdking/src/Components/Contact/Contact.js
+++ mvdking/src/Components/Contact/Contact.js
@@ -1,12 +1,65 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
+import { Form, Button, FormGroup, FormControl, ControlLabel,FloatingLabel } from "react-bootstrap";
+
+
 
 import './Contact.css'
 
 function Contact() {
   return (
-    <div>
-      Contact
-    </div>
+    <div className='contactpage'>
+                <h2 className='contactme'>Contact Me </h2>
+
+                <div className="textForm">
+
+                    <div className="text">
+                        <h3>Get in touch</h3>
+                        <br />
+
+                        <h4 className='email'>Email: seletematome3@gmail.com</h4>
+                        <br />
+                        <h4 className='phone'>Phone: (+27) 76-946-9953</h4>
+                        <br />
+            
+                    </div>
+
+                     <div className="forms">
+
+                        <div className="form">
+
+                            <Form action="https://formsubmit.co/seletematome3@gmail.com" method="POST">
+                                <div className='nameEmail'>
+                                    <Form.Group className="mb-3" controlId="formBasicName">
+                                        <input style={{ height: '50px', width: '100%', color: 'white' }} className='formname' type="text" name="name" required placeholder="Name" />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <input style={{ height: '50px', width: '100%', color: 'white' }} className='formemail' type="email" name="email" required placeholder="Email" />
+                                    </Form.Group>
+                                </div>
+                                <>
+                                    <FloatingLabel controlId="floatingTextarea2" >
+                                        <textarea
+                                            name='message'
+                                            as="textarea"
+                                            placeholder="Leave a comment here"
+                                            style={{ height: '200px', width: '100%', color: 'white' }}
+                                            className='formtext'
+                                        />
+                                    </FloatingLabel>
+                                    <Button className='submitbtn' type="submit">
+                                        Submit
+                                    </Button>
+                                </>
+                            </Form>
+
+                        </div> 
+
+
+                    </div>
+                </div>
+            </div>
   )
 }
 
--- mvdking/src/Components/Home/Home.js
+++ mvdking/src/Components/Home/Home.js
@@ -4,7 +4,7 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="container">
+    <div className="container Home">
       <section className="homebg">
         <div className="carousel w-full">
           <div id="slide1" className="carousel-item relative w-full">
@@ -69,8 +69,7 @@ function Home() {
         </div>
       </section>
 
-
-      <body>
+      <main className="homeMain">
         <div className="card w-96 bg-base-100 shadow-xl">
           <figure>
             <img
@@ -82,10 +81,91 @@ function Home() {
             <h2 className="card-title">Shoes!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+              <button
+                className="btn btn-primary"
+                onClick={() => window.my_modal_4.showModal()}
+              >
+                open modal
+              </button>
             </div>
           </div>
         </div>
+
+        {/*================================= You can open the modal using ID.showModal() method =========================*/}
+        <dialog id="my_modal_4" className="modal">
+          <form method="dialog" className="modal-box w-11/12 max-w-5xl">
+            
+          <div className="carousel w-small">
+          <div id="slide1" className="carousel-item relative w-full">
+            <img
+              src="https://www.side-step.co.za/media/catalog/product/cache/60023b40f56fdff39b9c495b8e044aef/a/d/add6620b-adidas--adv-hw-grxt-m-black-iq5325-v1_jpg_1.jpg"
+              className="w-full"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide4" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide2" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+
+          <div id="slide2" className="carousel-item relative w-full">
+            <img
+              src="https://www.side-step.co.za/media/catalog/product/cache/60023b40f56fdff39b9c495b8e044aef/p/m/pma4769gr-puma-ts-graphics-legacy-tee-green-62487743-v1_jpg_1.jpg"
+              className="w-full"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide3" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+
+          <div id="slide3" className="carousel-item relative w-full">
+            <img
+              src="https://www.side-step.co.za/media/catalog/product/cache/60023b40f56fdff39b9c495b8e044aef/a/d/add6620b-adidas--adv-hw-grxt-m-black-iq5325-v1_jpg_1.jpg"
+              className="w-full"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide2" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide4" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+
+          <div id="slide4" className="carousel-item relative w-full">
+            <img
+              src="https://www.side-step.co.za/media/catalog/product/cache/60023b40f56fdff39b9c495b8e044aef/p/m/pma4769gr-puma-ts-graphics-legacy-tee-green-62487743-v1_jpg_1.jpg"
+              className="w-full"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide3" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide1" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+        </div>
+
+            <p className="py-4">Click the button below to close</p>
+            <div className="modal-action">
+              {/*==================== if there is a button, it will close the modal ====================*/}
+              <button className="btn">Close</button>
+            </div>
+          </form>
+        </dialog>
+
+
 
         <div className="card w-96 bg-base-100 shadow-xl">
           <figure>
@@ -166,7 +246,7 @@ function Home() {
             </div>
           </div>
         </div>
-      </body>
+      </main>
     </div>
   );
 }
