6b93bc38ae5dce64c7b1a86bc98a210d4091239a
Matome <seletematome3@gmail.com>
2023-08-28 13:21:04 +0200

    desktop view

--- mvdking/src/Components/Home/Home.js
+++ mvdking/src/Components/Home/Home.js
@@ -4,72 +4,16 @@ import "./Home.css";
 
 function Home() {
   return ( 
-    <div className="container Home">
-      <section className="homebg">
-        <div className="carousel w-full">
-          <div id="slide1" className="carousel-item relative w-full">
-            <img
+    <div className=" Home">
+      <section className="homebg"> 
+            {/* <img
               src="https://www.side-step.co.za/media/catalog/product/cache/60023b40f56fdff39b9c495b8e044aef/a/d/add6620b-adidas--adv-hw-grxt-m-black-iq5325-v1_jpg_1.jpg"
-              className="w-full"
-            />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide4" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide2" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
+              className=""
+            /> */}
 
-          <div id="slide2" className="carousel-item relative w-full">
-            <img
-              src="https://www.side-step.co.za/media/catalog/product/cache/60023b40f56fdff39b9c495b8e044aef/p/m/pma4769gr-puma-ts-graphics-legacy-tee-green-62487743-v1_jpg_1.jpg"
-              className="w-full"
-            />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide1" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide3" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-
-          <div id="slide3" className="carousel-item relative w-full">
-            <img
-              src="https://www.side-step.co.za/media/catalog/product/cache/60023b40f56fdff39b9c495b8e044aef/a/d/add6620b-adidas--adv-hw-grxt-m-black-iq5325-v1_jpg_1.jpg"
-              className="w-full"
-            />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide2" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide4" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-
-          <div id="slide4" className="carousel-item relative w-full">
-            <img
-              src="https://www.side-step.co.za/media/catalog/product/cache/60023b40f56fdff39b9c495b8e044aef/p/m/pma4769gr-puma-ts-graphics-legacy-tee-green-62487743-v1_jpg_1.jpg"
-              className="w-full"
-            />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide3" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide1" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-        </div>
       </section>
 
-      <main className="homeMain">
+      <main className="container homeMain">
         <div className="card w-96 bg-base-100 shadow-xl">
           <figure>
             <img
@@ -91,7 +35,7 @@ function Home() {
           </div>
         </div>
 
-        {/*================================= You can open the modal using ID.showModal() method =========================*/}
+        {/*================================= You can open the modal using ID.showModal() method ========================= */}
         <dialog id="my_modal_4" className="modal">
           <form method="dialog" className="modal-box w-11/12 max-w-5xl">
             
