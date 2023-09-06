f9f0227932d219adcecae247e10711986651f775
Matome <seletematome3@gmail.com>
2023-09-06 22:03:28 +0200

    new backend

--- mvdkingBackend/mvdBackend/app.js
+++ mvdkingBackend/mvdBackend/app.js
@@ -1,43 +1,54 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
+const app = express();
 
-var app = express();
+// Set up Global configuration access
+dotenv.config();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+let PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+console.log(`Server is up and running on ${PORT} ...`);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Main Code Here //
+// Generating JWT
+app.post("/user/generateToken", (req, res) => {
+	// Validate User Here
+	// Then generate JWT Token
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	let jwtSecretKey = process.env.JWT_SECRET_KEY;
+	let data = {
+		time: Date(),
+		userId: 12,
+	}
+
+	const token = jwt.sign(data, jwtSecretKey);
+
+	res.send(token);
 });
 
-module.exports = app;
+// Verification of JWT
+app.get("/user/validateToken", (req, res) => {
+	// Tokens are generally passed in header of request
+	// Due to security reasons.
+
+	let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+	let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+	try {
+		const token = req.header(tokenHeaderKey);
+
+		const verified = jwt.verify(token, jwtSecretKey);
+		if(verified){
+			return res.send("Successfully Verified");
+		}else{
+			// Access Denied
+			return res.status(401).send(error);
+		}
+	} catch (error) {
+		// Access Denied
+		return res.status(401).send(error);
+	}
+});
