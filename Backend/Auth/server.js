06b6bd270a02cf60541ba327e717b8e60accb540
Matome <seletematome3@gmail.com>
2023-09-12 09:54:54 +0200

    fixing ther Cannot find module '/Users/da_m1_35/Desktop/MvdkingGraphics/Backend/Auth/index.js error

---
+++ Backend/Auth/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+
+const app = express();
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "bezkoder-session",
+    keys: ["COOKIE_SECRET"], // should use as secret environment variable
+    httpOnly: true,
+  })
+);
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+
+
